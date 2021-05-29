c5a6393c78b3ab4168c14735202b399a9024edf8
afiffudin-au <afiffudin077@gmail.com>
2021-05-29 16:52:40 +0700

    update banner

--- src/components/Banner/RadioButtonsGroup/RadioButtonsGroup.tsx
+++ src/components/Banner/RadioButtonsGroup/RadioButtonsGroup.tsx
@@ -1,20 +1,22 @@
 import { useState } from 'react'
 import style from './RadioButtonsGroup.module.scss'
-export default function RadioButtonsGroup() {
-  const [isChecked, setIsChecked] = useState<boolean>(false)
-  const handleCheck = () => {
-    setIsChecked(!isChecked)
-  }
+export default function RadioButtonsGroup({
+  handleCheck,
+  isChecked,
+}: {
+  handleCheck: any
+  isChecked: boolean
+}) {
   return (
     <div className={style.container}>
       <form>
         <label>
           <input type='radio' name='radio' checked={isChecked || true} />
-          <span onClick={handleCheck}>Icons</span>
+          <span onClick={() => handleCheck('icons')}>Icons</span>
         </label>
         <label>
           <input type='radio' name='radio' checked={isChecked} />
-          <span onClick={handleCheck}>Packs</span>
+          <span onClick={() => handleCheck('packs')}>Packs</span>
         </label>
       </form>
     </div>
--- src/components/Banner/SearchBanner/SearchBanner.tsx
+++ src/components/Banner/SearchBanner/SearchBanner.tsx
@@ -5,21 +5,34 @@ import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown'
 import RadioButtonsGroup from '../RadioButtonsGroup/RadioButtonsGroup'
 function SearchBanner() {
   const [isOpenMenu, setIsOpenMenu] = useState<boolean>(false)
+  const [typeToSearch, setTypeToSearch] = useState<string>('icons')
+  const [isChecked, setIsChecked] = useState<boolean>(false)
+  const handleCheck = (type: string) => {
+    setIsChecked(!isChecked)
+    setTypeToSearch(type)
+    setIsOpenMenu(false)
+  }
   const handleOpen = () => {
     setIsOpenMenu(!isOpenMenu)
   }
+  console.log(typeToSearch)
   return (
     <>
       <form className={style.searchBanner} action='/'>
         <div className={style.searchBar}>
           <div className={style.dropdownWrap}>
             <div className={style.dropdown} onClick={handleOpen}>
-              Icons
+              {typeToSearch}
               <div>
                 <ArrowDropDownIcon style={{ color: 'grey' }} />
               </div>
             </div>
-            {isOpenMenu && <RadioButtonsGroup />}
+            {isOpenMenu && (
+              <RadioButtonsGroup
+                handleCheck={handleCheck}
+                isChecked={isChecked}
+              />
+            )}
           </div>
 
           <input className={style.searchInput} type='text' />
