c69d3a61fd1f17cd7a8051eeb517b54f34ce0896
afiffudin-au <afiffudin077@gmail.com>
2021-06-01 21:10:41 +0700

    pagenation

--- src/components/IconPacks/IconPacks.tsx
+++ src/components/IconPacks/IconPacks.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import GridContainer from '../GridContainer/GridContainer'
 import style from './IconPacks.module.scss'
 import { useGetIconPacks } from '../../custom-hooks/useGetIconPacks/useGetIconPacks'
 import { useAppSelector } from '../../redux/app/hooks'
 import { selectTokenBlocks } from '../../redux/features/icon/iconSlice'
 import CardIconPacks from '../CardIconPack/CardIconPack'
+import { CircularProgress } from '@material-ui/core'
 interface IconPacksItems {
   catagory: string
   id: number
@@ -19,26 +20,62 @@ interface IconPacksItems {
 }
 function IconPacks() {
   const { token, tokenAccepted } = useAppSelector(selectTokenBlocks)
-  const { getIconPacks, dataIcons } = useGetIconPacks()
+  const [page, setPage] = useState<number>(1)
+  const { getIconPacks, dataIcons, isLoading } = useGetIconPacks()
   useEffect(() => {
     if (token) {
       getIconPacks(token)
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [tokenAccepted])
+  const handlePagenation = (numberOfPage: number) => {
+    if (numberOfPage === 0) {
+      setPage(1)
+    }
+    if (numberOfPage > 15) {
+      alert('Sorry : 15 page is max results')
+      return
+    }
+    setPage(numberOfPage)
+    getIconPacks(token, numberOfPage)
+  }
   return (
     <div className={style.iconPacks}>
       <h3 className={style.title}>Popular Icon Packs</h3>
+      {isLoading && (
+        <div className={style.loading}>
+          <CircularProgress />
+        </div>
+      )}
       <GridContainer>
-        {dataIcons?.data?.map((item: IconPacksItems, index: number) => (
-          <CardIconPacks
-            id={item.id}
-            image={item.images.sprite}
-            description={item.description}
-            numberOfIcons={item.pack_items}
-          />
-        ))}
+        {!isLoading &&
+          dataIcons?.data?.map((item: IconPacksItems, index: number) => (
+            <CardIconPacks
+              id={item.id}
+              image={item.images.sprite}
+              description={item.description}
+              numberOfIcons={item.pack_items}
+            />
+          ))}
       </GridContainer>
+      {!isLoading && (
+        <div className={style.pagenation}>
+          <div
+            style={{ display: page === 1 ? 'none' : 'block' }}
+            onClick={() => handlePagenation(page - 1)}
+            className={style.arrowBack}>
+            <p>Previous</p>
+          </div>
+          <div>
+            <p className={style.pageNumber}>{page}</p>
+          </div>
+          <div
+            onClick={() => handlePagenation(page + 1)}
+            className={style.arrowForward}>
+            <p>Next</p>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
